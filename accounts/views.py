08566cedf3f4202f99eca8dbd7ecfe26b284f46e
KaiCarlson <kai228carlson@gmail.com>
2025-09-07 21:13:32 -0400

    third try

--- accounts/views.py
+++ accounts/views.py
@@ -32,17 +32,15 @@ def signup(request):
     template_data['title'] = 'Sign Up'
     if request.method == 'GET':
         template_data['form'] = CustomUserCreationForm()
-        return render(request, 'accounts/signup.html',
-            {'template_data': template_data})
+        return render(request, 'accounts/signup.html', {'template_data': template_data})
     elif request.method == 'POST':
-        form = CustomUserCreationForm(request.POST,
-            error_class=CustomErrorList)
+        form = CustomUserCreationForm(request.POST, error_class=CustomErrorList)
         if form.is_valid():
             form.save()
             return redirect('accounts.login')
-    else:
-        template_data['form'] = form
-        return render(request, 'accounts/signup.html', {'template_data' : template_data})
+        else:
+            template_data['form'] = form
+            return render(request, 'accounts/signup.html', {'template_data': template_data})
 @login_required
 def orders(request):
     template_data = {}
